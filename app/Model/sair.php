95b5a63ac1c0667fd4593fc851bcc38f54b6e708
kaua.macahado <kaua.macahado@alunoescolassociais.org.br>
2022-10-04 17:26:24 -0300

    Logar usuário

--- app/Model/sair.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-ob_start();
-unset($_SESSION['id_usuario'], $_SESSION['nome_usuario']);
-$_SESSION['msg'] = "<p style='color: green'>Deslogado com sucesso!</p>";
-
-header("Location: index.html");
-?>
