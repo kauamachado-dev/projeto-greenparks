7e1b711d15dd629298dfb6c746f10cc6c143ed16
Stephane Cristine <boas.stephane@alunoescolassociais.org.br>
2022-10-18 17:31:10 -0300

    adicionado carrossel e mais 5 modal

--- app/Controller/_js/modal.js
+++ app/Controller/_js/modal.js
@@ -1,6 +1,21 @@
+//FUNÇÃO PARA ABRIR O MODAL
 function abrirModal(){
     document.getElementById('modal').style.top = "0";
 }
+//FUNÇÃO PARA FECHAR O MADAL
 function fecharModal(){
     document.getElementById('modal').style.top = "-100%";
 }
+function abrirModal2(){
+    document.getElementById('modal2').style.top = "0";
+  }
+  function fecharModal2(){
+    document.getElementById('modal2').style.top = "-100%";
+  } 
+
+  function abrirModal3(){
+    document.getElementById('modal3').style.top = "0";
+  }
+  function fecharModal3(){
+    document.getElementById('modal3').style.top = "-100%";
+  } 
