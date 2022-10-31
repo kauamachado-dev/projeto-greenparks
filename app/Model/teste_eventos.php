624b7dd1f2cb25373706cd3dde288954f011ccd6
stephcristine <boas.stephane@alunoescolassociais.org.br>
2022-10-31 14:36:12 -0300

    atualização de arquivos

--- app/Model/teste_eventos.php
+++ app/Model/teste_eventos.php
@@ -11,7 +11,6 @@
   <body>
   <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">cadastro</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
