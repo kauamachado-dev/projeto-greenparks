7c96537959ce22b5f76e507dbed909f595eab963
fatima.paulino <fatima.paulino@alunoescolassociais.org.br>
2022-11-07 17:18:03 -0300

    Index e sliders.

--- app/Controller/_js/slider.js
+++ app/Controller/_js/slider.js
@@ -1,25 +1,25 @@
-let time1 = 5000, //tempo de troca de imagem
-    currentImageIndex1 = 0, //primeira imagem a aparecer que possui index = 0
-    images1 = document.querySelectorAll("#slider img") // pega todas as imagens dentro da div slider
-    max1 = images1.length;
+let time2 = 3000, //tempo de troca de imagem
+    currentImageIndex2 = 0, //primeira imagem a aparecer que possui index = 0
+    images2 = document.querySelectorAll("#sliderau img") // pega todas as imagens dentro da div slider
+    max2 = images2.length;
 //função para trocar as imagens
-function nextImage() {
+function nextImage_2() {
 
-    images1[currentImageIndex1].classList.remove("selected")//remove a imagem que estiver com "class =  selected1"
+    images2[currentImageIndex2].classList.remove("selectedau")//remove a imagem que estiver com "class =  selected1"
 
-    currentImageIndex1++ 
+    currentImageIndex++ 
 
-    if(currentImageIndex1 >= max1)
-        currentImageIndex1 = 0
+    if(currentImageIndex2 >= max2)
+       currentImageIndex2 = 0
 
-    images1[currentImageIndex1].classList.add("selected")//mostra a nova imagem com "class = selected1"
+    images2[currentImageIndex2].classList.add("selectedau")//mostra a nova imagem com "class = selected1"
 }
 // função que inicia a troca de imagens 
-function start() {
+function start_2() {
     setInterval(() => {
-        // faz a troca de imagens
-        nextImage()
-    }, time1)
+         //faz a troca de imagens
+        nextImage_2()
+    }, time2)
 }
 // inicia o slider assim que a página carregar
-window.addEventListener("load", start)
+window.addEventListener("load", start_2)
--- app/Controller/_js/sliderev.js
+++ app/Controller/_js/sliderev.js
@@ -1,25 +1,25 @@
-//let time1 = 5000, //tempo de troca de imagem
-   /// currentImageIndex1 = 0, //primeira imagem a aparecer que possui index = 0
-   // images1 = document.querySelectorAll("#sliderev img") // pega todas as imagens dentro da div slider
-   // max1 = images1.length;
+let time1 = 3000, //tempo de troca de imagem
+    currentImageIndex1 = 0, //primeira imagem a aparecer que possui index = 0
+    images1 = document.querySelectorAll("#sliderev img") // pega todas as imagens dentro da div slider
+    max1 = images1.length;
 //função para trocar as imagens
-//function nextImage() {
+function nextImage() {
 
-    //images1[currentImageIndex1].classList.remove("selected1")//remove a imagem que estiver com "class =  selected1"
+    images1[currentImageIndex1].classList.remove("selectedev")//remove a imagem que estiver com "class =  selectedev"
  
-  // currentImageIndex1++ 
+   currentImageIndex1++ 
 
-   // if(currentImageIndex1 >= max1)
-       // currentImageIndex1 = 0
-//
-    //images1[currentImageIndex1].classList.add("selected1")//mostra a nova imagem com "class = selected1"
-//
+    if(currentImageIndex1 >= max1)
+       currentImageIndex1 = 0
+
+    images1[currentImageIndex1].classList.add("selectedev")//mostra a nova imagem com "class = selectedev"
+}
 // função que inicia a troca de imagens 
-//function start() {
-    //setInterval(() => {
-        // faz a troca de imagens
-      //  nextImage()
-    //}, time1)
-//}
-// inicia o slider assim que a página carregar
-//window.addEventListener("load", start)
+function start() {
+    setInterval(() => {
+         //faz a troca de imagens
+        nextImage()
+    }, time1)
+}
+//inicia o slider assim que a página carregar
+window.addEventListener("load", start)
