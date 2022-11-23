7c3875749501dbf68cefb49d148ca2a6a82a6165
fatima.paulino <fatima.paulino@alunoescolassociais.org.br>
2022-11-23 14:05:43 -0300

    teste

--- app/Model/login.php
+++ app/Model/login.php
@@ -26,38 +26,47 @@ $u = new Usuario;
     </head>
     <body id="body">
         <!-- TELA DE LOGIN -->
-        <!-- TELA DE LOGIN -->
-        <div class="container">
-                                    <div class="card">
-                                    <div class="titulo1">INSTRUTOR</div>
+        <section class="h-100 gradient-form">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-xl-10">
+                        <div class="card rounded-3 text-black">
+                            <div class="row g-0">
+                                <div class="col-lg-6">
+                                    <div class="card-body p-md-5 mx-md-4">
                                         <!-- FORMULÁRIO -->
-                                        <form method="POST" action="processa.php">
+                                        <form method="POST">
+                                        <form action="processa.php" method="POST">
                                             <!-- USUÁRIO -->
-                                            <div class="label-float">
-                                                <input type="text" class="form-control" id="nome_usuario" required name="usuario">
-                                                <label for="usuario">Usuário </label>
+                                            <div class="form-floating mb-3">
+                                                <input type="text" class="form-control" id="nome_usuario" required name="usuario" maxlength="75" placeholder="Digite o usuário">
+                                                <input type="text" class="form-control" id="nome_usuario" required name="nome_usuario" maxlength="75" placeholder="Digite o usuário">
+                                                <label for="usuario">Usuário <i class="fas fa-user"></i> </label>
                                             </div>
                                             <!-- SENHA -->
-                                            <div class="label-float">
-                                                <input type="password" class="form-control" id="senha_usuario" required name="senha">
-                                                <label for="senha"> Senha  </label>
+                                            <div class="form-floating mb-3">
+                                                <input type="password" class="form-control" id="senha_usuario" required name="senha" maxlength="75" placeholder="Digite a senha">
+                                                <input type="password" class="form-control" id="senha_usuario" required name="senha_usuario" maxlength="75" placeholder="Digite a senha">
+                                                <label for="senha"> Senha <i class="fas fa-key"></i> </label>
                                                 <div id="olho"><abbr title="Mostrar senha" id="##"><i class="fas fa-eye-slash" id="btn-eye" onclick="mostrar()"></i></abbr></div>
                                             </div>
                                             <!-- BOTÃO -->
-                                            <div class="btn">
-                                                <a type="submit" value="ACESSAR" class="btn1"> ACESSAR </a>
+                                            <div class="text-center pt-1 mb-5 pb-1">
+                                                <input type="submit" value="ACESSAR" class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3 mt-4">
                                             </div>
-                                            <div>
-                                                <hr>
-                                            </div>
-                                            <p> Não tem uma conta?
-                                                <a href=''> Cadastre-se! </a>
                                         </form>
                                     </div>
+                                </div>
+                                        <h4 class="mb-4">BEM VINDO</h4>
+                                        <p class="mb-4">
+                                        </p>
                                     </div>
-                                    </div>
-
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </body>
-</html>
 </html>
