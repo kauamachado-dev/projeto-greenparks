f6a752ea9fd657fb6c992d213c281754b9a3cbc0
fatima.paulino <fatima.paulino@alunoescolassociais.org.br>
2022-12-07 15:07:46 -0300

    Design novo evento e nova oficina.

--- app/Model/eventos/novo_evento.php
+++ app/Model/eventos/novo_evento.php
@@ -8,29 +8,29 @@
     <input type="hidden" name="acao" value="cadastrar">
     <h2 style="font-family:'Passion One'; margin-top:2%; font-size:40px; color:#09400A">Dados sobre o evento</h2>
     <div class="mb-3">
-        <label>Nome do Evento:</label>
+        <label style="font-family:'Arimo'; font-size: 20px;">Nome do Evento:</label>
         <input type="text" name="nome_eventos" class="form-control">
     </div>
     <div class="mb-3">
-        <label>Data do Evento:</label>
+        <label style="font-family:'Arimo'; font-size: 20px;">Data do Evento:</label>
         <input type="date" name="data_eventos" class="form-control">
     </div>
     <div class="mb-3">
-        <label>Horario do Evento:</label>
+        <label style="font-family:'Arimo'; font-size: 20px;">Horario do Evento:</label>
         <input type="time" name="horario_eventos" class="form-control">
     </div>
     <div class="mb-3">
-        <label>Descrição do Evento:</label>
+        <label style="font-family:'Arimo'; font-size: 20px;">Descrição do Evento:</label>
         <input type="text" name="desc_eventos" class="form-control">
     </div>
     <br><br>
     <h2 style="font-family:'Passion One'; margin-top:2%; font-size:40px; color:#09400A">Local do evento</h2>
     <div class="mb-3">
-        <label>CEP:</label>
+        <label style="font-family:'Arimo'; font-size: 20px;">CEP:</label>
         <input type="text" name="cep_eventos" class="form-control">
     </div>
     <div class="mb-3">
-        <label>Número da casa/local:</label>
+        <label style="font-family:'Arimo'; font-size: 20px;">Número da casa/local:</label>
         <input type="text" name="num_ende_eventos" class="form-control">
     </div>
     <button type="submit" class="btn btn-success">Enviar</button>
