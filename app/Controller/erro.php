ade0b580fd2466fada1c565d99a51ec55f3cc23f
kaua.macahado <kaua.macahado@alunoescolassociais.org.br>
2022-12-07 14:00:47 -0300

    Pagina de erro

--- app/Controller/erro.php
+++ app/Controller/erro.php
@@ -1,4 +1,4 @@
-<p>erro</p>
+<h1>OPSSS....</h1>
 
 
-    <a class="btn btn-danger ml-3" href="../Controller/sair.php"><img src="../View/css/images/erro.png" style="width: 50%">Sair da conta</a>
+<a class="btn btn-danger ml-3" href="../Controller/sair.php"><img src="../View/css/images/erro.png" style="width: 50%">Sair da conta</a>
