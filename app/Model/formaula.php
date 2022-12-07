8e1bb88605be2787b05fa5088337cfd59a4d6953
fatima.paulino <fatima.paulino@alunoescolassociais.org.br>
2022-12-07 17:18:29 -0300

    pequenas alterações

--- app/Model/formaula.php
+++ app/Model/formaula.php
@@ -14,6 +14,7 @@ include("conexao.php");
   <meta charset='utf-8'>
   <title>FORMULÁRIO</title>
   <link rel="shortcut icon" href="../View/css/images/logo.png" type="image/x-icon">
+  <link href="../View/css/form_aula.css" rel="stylesheet">
 </head>
 <body>  
 <form action="cad_aula.php" method="POST">
@@ -21,14 +22,14 @@ include("conexao.php");
       <div class="row">
         <div class="col">
           <div class="single-input">
-            <label for="nome">Nome:</label>
+            <label for="nome" style="font-family:'Passion One'; font-size:60px; color:#128013; top:10%;">Nome:</label>
             <input required type="text" name="nome_usuario" id="nome" class="input" value="<?php print $row->nome_usuario ?>";>
             <input type="hidden" name="id_usuario" value="<?php print $row->id_usuario ?>">
           </div>
         </div>
        <br> Oficinas:
             <select name="id_oficina">
-              <option>Selecione</option>
+              <option >Selecione</option>
                 <?php
                   $sql = "SELECT a.* , o.* FROM aula a LEFT JOIN oficina o ON a.id_oficina = o.id_oficina";
                   $result = mysqli_query($conexaoMysqli, $sql);
